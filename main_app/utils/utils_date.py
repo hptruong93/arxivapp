28ea699b393dbb391856ace9d10c5137d66d0b29
hptruong93 <hptruong93@gmail.com>
2016-05-12 23:04:33 -0400

    Change default parameter to not be static over time

--- main_app/utils/utils_date.py
+++ main_app/utils/utils_date.py
@@ -14,7 +14,10 @@ def string_to_date(string):
 def date_to_string(date, date_format = DEFAULT_DATE_FORMAT):
     return date.strftime(date_format)
 
-def previous_business_date(date = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0), count_back = 1):
+def previous_business_date(date = None, count_back = 1):
+    if date is None:
+        date = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
     delta = datetime.timedelta(days = 1)
     count_down = count_back
     while count_down > 0:
